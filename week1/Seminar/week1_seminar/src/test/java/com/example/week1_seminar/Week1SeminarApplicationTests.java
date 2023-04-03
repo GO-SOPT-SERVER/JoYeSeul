262c56c41b3c6edf070dc96befb712a291392e86
yeseul106 <20191037@sungshin.ac.kr>
2023-04-03 10:08:40 +0900

    #1 [WEEK1] 세미나 내용

---
+++ week1/Seminar/week1_seminar/src/test/java/com/example/week1_seminar/Week1SeminarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week1_seminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week1SeminarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
