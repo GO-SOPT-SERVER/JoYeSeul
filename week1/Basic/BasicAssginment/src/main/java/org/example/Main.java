9d3e8ce1b08334b094a7da4bdd7875935c33c4a7
yeseul106 <20191037@sungshin.ac.kr>
2023-04-18 03:21:38 +0900

    #1 [WEEK1] 기본 과제 제출

--- week1/Basic/BasicAssginment/src/main/java/org/example/Main.java
+++ week1/Basic/BasicAssginment/src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ import org.example.ClaasAndInstance.TV;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1주차 기본 과제");
+        System.out.println("1주차 기본 Basic 과제");
     }
 }
