ace951c660b77ebd54b4c097a07759b8ecd1359c
yeseul106 <20191037@sungshin.ac.kr>
2023-06-01 20:50:33 +0900

    [WEEK3] 기본 과제 제출

---
+++ week3/Basic/ThirdSeminar/src/test/java/com/example/ThirdSeminar/ThirdSeminarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThirdSeminar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdSeminarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
